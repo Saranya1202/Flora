aa3124a725f2d3813d21d81397353e1aa30d1eb6
Saranya1202 <141426065+Saranya1202@users.noreply.github.com>
2024-10-03 10:26:27 +0530

    Add files via upload

---
+++ Winter.php
@@ -0,0 +1,310 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <body>
+        <header>
+            <h1>WINTER</h1>
+        </header>
+        <li><a href="superflora.html">Home</a></li>
+         <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://wallpapercave.com/wp/wp6506706.jpg" class="d-block w-100" alt="https://wallpapercave.com/wp/wp6506706.jpg">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Agriculture is the backbone of India</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+          <img src="https://p4.wallpaperbetter.com/wallpaper/9/904/959/field-nature-land-wheat-wallpaper-preview.jpg" class="d-block w-100" alt="https://p4.wallpaperbetter.com/wallpaper/9/904/959/field-nature-land-wheat-wallpaper-preview.jpg">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Agritechnology</h5>
+            </div>
+        </div>
+        <div class="carousel-item">
+          <img src="https://media.istockphoto.com/id/870915532/photo/man-holding-crate-ob-fresh-vegetables.jpg?s=612x612&w=0&k=20&c=k2dXOI-wxUy7lX77Pm90vU6TJXmAAv5VtK60ZZHIyCA="class="d-block w-100" alt="https://media.istockphoto.com/id/870915532/photo/man-holding-crate-ob-fresh-vegetables.jpg?s=612x612&w=0&k=20&c=k2dXOI-wxUy7lX77Pm90vU6TJXmAAv5VtK60ZZHIyCA=">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>House of love in agriculture </h5>
+            </div>
+        </div>
+      </div>
+      </div>
+          <div class="container">
+            <div class="analysis-section">
+                <h2 style="font-size: 35px;">How to choose your crops of winter?</h2>
+                <div class="mnc py-4">
+                <div class="col">
+                <div><h3><p class="k"><p style="font-size: 25px;">Choose crop varieties that are well-suited to your local climate and are more tolerant of cold temperatures. Cold-hardy crops are less susceptible to winter damage. Mulching: Apply a layer of organic mulch, such as straw, leaves, or compost, around the base of your crops.</h3></p>
+             </div>
+             </div>
+             </div>
+             </div>
+             </div>
+             
+             <div><html>
+    <head>
+    <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+    
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+    </style>
+    </head>
+    
+    
+    <table>
+      <tr>
+        <th>Vegetables To Grow</th>
+        <th>Growing Months</th>
+        <th>Days to Harvest</th>
+           <th>Shop seeds</th>
+      </tr>
+        <tr>
+        <td>Beetroot</td>
+        <td>Oct - Nov</td>
+        <td>80 - 90</td>
+         <td>Beetroot seeds</td>
+      </tr>
+     <tr>
+        <td>Broccoli</td>
+        <td>Sept - Nov</td>
+        <td>90-100</td>
+         <td>Broccoli seeds</td>
+      </tr>
+     <tr>
+        <td>Brussel Sprouts</td>
+        <td>Aug - Oct</td>
+        <td>160 - 180</td>
+         <td>Brussel Sprouts seeds</td>
+      </tr>
+     <tr>
+        <td>Cabbage</td>
+        <td>Sep - Nov</td>
+        <td>80 - 90</td>
+         <td>Cabbage seeds</td>
+      </tr>
+     <tr>
+        <td>Carrot</td>
+        <td>Aug - Oct</td>
+        <td>70 - 80</td>
+         <td>Carrot seeds</td>
+      </tr>
+     <tr>
+        <td>Cauliflower</td>
+        <td>Sep - Nov</td>
+        <td>110- 120</td>
+         <td>Cauliflower seeds</td>
+      </tr>
+       <tr>
+        <td>Kale</td>
+        <td>Sep - Nov</td>
+        <td>60 - 90</td>
+         <td>Kale seeds</td>
+      </tr>
+     <tr>
+        <td>Kohirabi</td>
+        <td>Sep - Oct</td>
+        <td>60 -  70</td>
+         <td>Kohirabi seeds</td>
+      </tr>
+     <tr>
+        <td>Leek</td>
+        <td>Aug - Oct</td>
+        <td>80 - 90</td>
+         <td>Leek seeds</td>
+      </tr>
+     <tr>
+        <td>Lettuce</td>
+        <td>Sep- Nov</td>
+        <td>40 - 50</td>
+         <td>Lettuce seeds</td>
+      </tr>
+     <tr>
+        <td>Onion</td>
+        <td>Sep - Nov</td>
+        <td>150 - 160 </td>
+         <td>Onion seeds</td>
+      </tr>
+     <tr>
+        <td>Peas</td>
+        <td>Sep  - Oct</td>
+        <td>55- 60</td>
+         <td>Peas seeds</td>
+      </tr>
+     <tr>
+        <td>Radish</td>
+        <td>All seasons*</td>
+        <td>45 - 50</td>
+         <td>Radish seeds</td>
+      </tr>
+     <tr>
+        <td>Spinach</td>
+        <td>All Seasons*</td>
+        <td>50 - 60</td>
+         <td>Spinach seeds</td>
+      </tr>
+             </div>
+     
+      
+      
+        <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <img class="rounded" src="https://images.unsplash.com/photo-1576045057995-568f588f82fb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8c3BpbmFjaHxlbnwwfHwwfHx8MA%3D%3D"height="100%" width="75%"/>
+        </div>
+        <div class="col">
+          <h1 style="color:blue">Spinach<span style="color:#4dff4d">Cultivation</span></h1>
+          <h2><p class="k">September to October is the optimal period for planting this vegetable. It can be cultivated on any kind of soil with adequate drainage. But when planted on sandy loam and alluvial soil, it produces good resultsLine sowing and broadcasting are two sowing techniques. Use a plant-to-plant distance of 5–10 cm and a row-to-row distance of 25–30 cm.
+    Depending on the variety, the crop is ready for first cutting 25–30 days after sowing.
+    
+        </div>
+      </div>
+    </div> 
+    <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <img class="rounded" src="https://media.istockphoto.com/id/185275579/photo/bundles-of-organic-carrots-with-the-stems-still-attached.webp?b=1&s=170667a&w=0&k=20&c=tlOo0x-fKU2H1tz58_11VC2tiZwlDYf-icMR5dZviDU="height="100%" width="75%"/>
+        </div>
+        <div class="col">
+          <h1 style="color:blue">Carrot<span style="color:#4dff4d">Cultivation</span></h1>
+          <h2><p class="k">Deep, loose loamy soils are necessary for carrots to develop their roots properly. The pH of the soil should be between 5.5 and 7 for maximum production.
+    Raised beds or ridged beds are used for direct seeding.
+    Depending on the cultivar, edible roots can be harvested 80 to 100 days after the seeds are sown.</p> </h2> 
+    
+    
+        </div>
+      </div>
+    </div> 
+    <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <img class="rounded" src="https://rukminim2.flixcart.com/image/750/900/xif0q/plant-seed/o/7/w/260-radish-mooli-hybrid-vegetable-seeds-r3398-kanaya-original-imaggy5haspbajry.jpeg?q=20&crop=false"height="100%" width="75%"/>
+        </div>
+        <div class="col">
+          <h1 style="color:blue">Raddish<span style="color:#4dff4d">Cultivation</span></h1>
+          <h2><p class="k">Another popular winter vegetable, radish is a great option for cultivation throughout the colder months.
+    It can be cultivated on any type of soil, however light friable, sandy loam soil produces the greatest results. The best time to sow winter varieties of radish (Japanese white) is between November and December. Line sowing and broadcasting are two sowing techniques.</p></h2>
+    
+    
+        </div>
+      </div>
+    </div> 
+    <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <img class="rounded" src="https://media.istockphoto.com/id/477419224/photo/fresh-cabbage-cross-section-with-water-drops.webp?b=1&s=170667a&w=0&k=20&c=RyQdvF1XGdXpIN6d_khDFWfSJ6pjvTBcdDmptx6meg8="height="100%" width="75%"/>
+        </div>
+        <div class="col">
+          <h1 style="color:blue">Cabbage<span style="color:#4dff4d">Cultivation</span></h1>
+          <h2><p class="k">Cabbage is primarily grown in the plains region of India throughout the winter. It performs best when planted on loamy soil that has strong moisture retention ability and is well drained. The pH of the soil should be between 5.5 and 6.5.The best planting season for plantations is from September to October. When the head is fully developed and has a hard texture, harvesting is safe.</p> </h2> 
+    
+    
+        </div>
+      </div>
+    </div> 
+    <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <img class="rounded" src="https://media.istockphoto.com/id/1063168360/photo/turnips.jpg?s=612x612&w=0&k=20&c=7xIFcXB1oOne8Mat8wgcTPNlKoUyglKYa5XsP-__8AE="height="100%" width="75%"/>
+        </div>
+        <div class="col">
+          <h1 style="color:blue">Turnip<span style="color:#4dff4d">Cultivation</span></h1>
+          <h2><p class="k">Turnip is primarily grown during the winter. This crop is unable to withstand extreme heat and inconsistent water supply. turnips can be grown in a wide range of soil types, they perform best on loamy soils that are rich in organic matter. For healthy growth, the soil should have a pH of 5.5 to 6.8.Turnip roots should be pulled out once they have reached the marketable size, which is between 5 and 10 cm in diameter, depending on the variety.
+    </p> </h2> 
+    
+    
+        </div>
+      </div>
+    </div> 
+    <div class="container py-5">
+      <div class="row">
+        <div class="col">
+          <img class="rounded" src="https://5.imimg.com/data5/SELLER/Default/2021/4/OQ/UM/UF/32597203/5-kg-green-coriander-seed-500x500.jpg"height="100%" width="75%"/>
+        </div>
+        <div class="col">
+          <h1 style="color:blue">Corriander<span style="color:#4dff4d">Cultivation</span></h1>
+          <h2><p class="k">It may grow on any sort of soil, but well-drained loamy soils are preferred. The pH range of the soil should be 8–10. The first week of October is the best time to sow vegetables.
+    Maintain a row-to-row distance of 30 cm and a plant-to-plant spacing of 15 cm. The first irrigation should be provided shortly after sowing. At intervals of 10 to 12 days, subsequent irrigations should be provided.
+    Harvest the vegetable when it reaches maturity and shows a good amount of leaf growth.</p> </h2> 
+    
+    
+        </div>
+      </div>
+    </div> 
+    
+    
+    
+        <h3>Select Your District in Tamil Nadu</h3>
+     
+    <html>
+      <head>
+          <title>Find Best Crop</title>
+          <style>
+              body {
+                  font-family: Arial, sans-serif;
+                  text-align: center;
+                 
+              }
+              .container {
+                  margin:0px;
+              }
+              #location-input {
+                  width: 400px;
+                  padding: 10px;
+                  font-size: 16px;
+              }
+              #search-button {
+                  padding: 10px 20px;
+                  font-size: 16px;
+                  background-color: #007BFF;
+                  color: #fff;
+                  border: none;
+                  cursor: pointer;
+                  align:center;
+              }
+              button {
+          border:none;
+          background-color:purple;
+          outline:none;
+          cursor:pointer;
+      }
+      
+          </style>
+      </head>
+      <?php
+      include_once('configwin.php');
+      ?>
+      <body>
+          <div class="container"  align="center;">
+              <br>
+              <h1 style="text-align:center;background-image: linear-gradient(to right, red,orange,yellow,green,blue,indigo,violet); ;width:115%;">Find the Best Crop for Your district</h1>
+              <form action='searchwin.php' method='POST' style="background-image: linear-gradient(red, yellow);width:115%;border-radius:20px;">
+              <br>
+                  <label for="location-input"><h2>Enter Your district:</h2></label>
+                  <input type="text" id="location-input" placeholder="Enter your district" name="fog" required>
+                  <br>
+                  <br>
+                  <button type="submit" style="height:50px;width:100px;border-radius:20px;">Search</button><br><br>
+      
+              </form>
+              <br>
+          </div>
+      
+          </body>
+      </body>
+      </html>
